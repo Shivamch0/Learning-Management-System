7cf2186816d553bbc070eb38cf5d3697cfbab47f
Shivam <choudharyshivam0508@gmail.com>
2026-06-24 13:50:20 +0530

    Refactor Navbar component to conditionally render user options based on authentication status

--- Frontend/src/components/student/Navbar.jsx
+++ Frontend/src/components/student/Navbar.jsx
@@ -1,8 +1,12 @@
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/react";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
+
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
 
   return (
     <div
@@ -15,20 +19,41 @@ const Navbar = () => {
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
-          <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollments</Link>
+          {user && (
+            <>
+              <button>Become Educator</button>
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-          Create Account
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+            onClick={() => openSignIn()}
+          >
+            Create Account
+          </button>
+        )}
       </div>
 
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
-        <div>
-          <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollments</Link>
+        <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+          {user && (
+            <>
+              <button>Become Educator</button>
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
         </div>
-        <button><img src={assets.user_icon} alt="" /></button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="" />
+          </button>
+        )}
       </div>
     </div>
   );
