99f45e33389b04aed2f42df668cb0826519f2b41
Shivam <choudharyshivam0508@gmail.com>
2026-06-24 15:07:47 +0530

    Add calculateRating function to AppContext; update CourseCard to display dynamic ratings and course ratings count

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from 'react-router-dom'
 
 export const AppContext = createContext();
 
 export const AppProvider = ({children}) => {
 
     const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate()
 
     const [allCourses , setAllCourses] = useState([]);
 
@@ -13,12 +15,23 @@ export const AppProvider = ({children}) => {
         setAllCourses(dummyCourses);
     }
 
+    const calculateRating = (course) => {
+        if(course.courseRatings.length === 0) return 0;
+
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        });
+
+        return totalRating / course.courseRatings.length
+    }
+
     useEffect(() => {
         fetchAllCourses();
     } , [])
 
     const value = {
-        currency , allCourses
+        currency , allCourses , navigate , calculateRating
     }
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>
