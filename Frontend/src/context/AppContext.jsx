ab9e376877e85b6059ec626c159278b247b43262
Shivam <choudharyshivam0508@gmail.com>
2026-06-25 10:49:23 +0530

    Feat: Enhance CourseDetails page with detailed course structure and duration calculations

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration'
 
 export const AppContext = createContext();
 
@@ -27,12 +28,35 @@ export const AppProvider = ({children}) => {
         return totalRating / course.courseRatings.length
     }
 
+    const calculateChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return  humanizeDuration(time * 60 * 1000 , { units : ['h' , 'm']});
+    }
+
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+
+        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration));
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+    }
+
+    const calculateNoOfLectures = (course) => {
+        let totalLecture = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLecture += chapter.chapterContent.length;
+            }
+        });
+        return totalLecture;
+    }
+
     useEffect(() => {
         fetchAllCourses();
     } , [])
 
     const value = {
-        currency , allCourses , navigate , calculateRating , isEducator , setIsEducator
+        currency , allCourses , navigate , calculateRating , isEducator , setIsEducator , calculateChapterTime , calculateCourseDuration , calculateNoOfLectures
     }
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>
