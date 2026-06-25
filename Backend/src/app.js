06b0ac2a7fe995c7e4dda9506829459b182ccf7b
Shivam <choudharyshivam0508@gmail.com>
2026-06-26 00:52:10 +0530

    Feat: Completed the webhook user authentication

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { clerkWebHooks } from './controller/webhooks.js';
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.post('/clerk' , express.json() , clerkWebHooks)
 
 
 export default app;
