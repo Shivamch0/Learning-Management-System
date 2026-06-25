872cb2210b50f99ec4fa64ba129df87e2789dc9c
Shivam <choudharyshivam0508@gmail.com>
2026-06-25 11:55:24 +0530

    Feat: Enhance MyEnrollments page with course details and progress tracking

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,63 +1,82 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
-import { useNavigate } from 'react-router-dom';
-import humanizeDuration from 'humanize-duration'
+import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
-export const AppProvider = ({children}) => {
+export const AppProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
 
-    const currency = import.meta.env.VITE_CURRENCY;
-    const navigate = useNavigate()
+  const [allCourses, setAllCourses] = useState([]);
+  const [isEducator, setIsEducator] = useState(true);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
-    const [allCourses , setAllCourses] = useState([]);
-    const [isEducator , setIsEducator] = useState(true);
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
 
-    const fetchAllCourses = async () => {
-        setAllCourses(dummyCourses);
-    }
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) return 0;
 
-    const calculateRating = (course) => {
-        if(course.courseRatings.length === 0) return 0;
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
 
-        let totalRating = 0;
-        course.courseRatings.forEach(rating => {
-            totalRating += rating.rating
-        });
+    return totalRating / course.courseRatings.length;
+  };
 
-        return totalRating / course.courseRatings.length
-    }
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+    chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration));
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
 
-    const calculateChapterTime = (chapter) => {
-        let time = 0;
-        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
-        return  humanizeDuration(time * 60 * 1000 , { units : ['h' , 'm']});
-    }
+  const calculateCourseDuration = (course) => {
+    let time = 0;
 
-    const calculateCourseDuration = (course) => {
-        let time = 0;
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map(
+        (lecture) => (time += lecture.lectureDuration),
+      ),
+    );
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
 
-        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration));
-        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
-    }
+  const calculateNoOfLectures = (course) => {
+    let totalLecture = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLecture += chapter.chapterContent.length;
+      }
+    });
+    return totalLecture;
+  };
 
-    const calculateNoOfLectures = (course) => {
-        let totalLecture = 0;
-        course.courseContent.forEach(chapter => {
-            if(Array.isArray(chapter.chapterContent)){
-                totalLecture += chapter.chapterContent.length;
-            }
-        });
-        return totalLecture;
-    }
+  const fetchUserEnrolledCourses = async () => {
+    setEnrolledCourses(dummyCourses);
+  };
 
-    useEffect(() => {
-        fetchAllCourses();
-    } , [])
+  useEffect(() => {
+    fetchAllCourses();
+    fetchUserEnrolledCourses();
+  }, []);
 
-    const value = {
-        currency , allCourses , navigate , calculateRating , isEducator , setIsEducator , calculateChapterTime , calculateCourseDuration , calculateNoOfLectures
-    }
+  const value = {
+    currency,
+    allCourses,
+    navigate,
+    calculateRating,
+    isEducator,
+    setIsEducator,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNoOfLectures,
+    enrolledCourses,
+    fetchUserEnrolledCourses,
+  };
 
-    return <AppContext.Provider value={value}>{children}</AppContext.Provider>
-}
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
