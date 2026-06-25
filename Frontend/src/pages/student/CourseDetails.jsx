11487426cdb5747dc975854cdb11320e14ed89d8
Shivam <choudharyshivam0508@gmail.com>
2026-06-25 11:18:54 +0530

    Feat: Update CourseDetails page with enhanced layout, course description, and enrollment button; add custom footer component

--- Frontend/src/pages/student/CourseDetails.jsx
+++ Frontend/src/pages/student/CourseDetails.jsx
@@ -4,17 +4,21 @@ import { AppContext } from "../../context/AppContext";
 import { assets } from "../../assets/assets";
 import Loading from "../../components/student/Loading";
 import humanizeDuration from "humanize-duration";
+import Footer from '../../components/student/Footer'
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSection] = useState({});
+  const [isAlreadYEnrolled, serIsAlreadyEnrolled] = useState(false);
+
   const {
     allCourses,
     calculateRating,
     calculateChapterTime,
     calculateCourseDuration,
     calculateNoOfLectures,
+    currency,
   } = useContext(AppContext);
 
   const fetchCourseData = async () => {
@@ -27,8 +31,8 @@ const CourseDetails = () => {
   }, []);
 
   const toggleSection = (index) => {
-    setOpenSection((prev) => ({...prev , [index] : !prev[index]}))
-  }
+    setOpenSection((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
 
   return courseData ? (
     <>
@@ -88,7 +92,11 @@ const CourseDetails = () => {
                     onClick={() => toggleSection(index)}
                   >
                     <div className="flex items-center gap-2">
-                      <img src={assets.down_arrow_icon} alt="arrow_icon" className={`transform transition-transform ${openSection[index] ? 'rotate-180' : ''}`} />
+                      <img
+                        src={assets.down_arrow_icon}
+                        alt="arrow_icon"
+                        className={`transform transition-transform ${openSection[index] ? "rotate-180" : ""}`}
+                      />
                       <p className="font-medium md:text-base text-sm">
                         {chapter.chapterTitle}
                       </p>
@@ -134,10 +142,84 @@ const CourseDetails = () => {
               ))}
             </div>
           </div>
+
+          <div className="py-20 text-sm md:text-default">
+            <h3 className="text-xl font-semibold text-gray-800">
+              Course Description
+            </h3>
+            <p
+              className="pt-3 rich-text"
+              dangerouslySetInnerHTML={{
+                __html: courseData.courseDescription,
+              }}
+            ></p>
+          </div>
         </div>
 
-        <div></div>
+        <div className="max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-75 sm:min-w-105">
+          <img src={courseData.courseThumbnail} alt="thumbnail" />
+          <div className="p-5">
+            <div className="flex items-center gap-2">
+              <img
+                src={assets.time_left_clock_icon}
+                alt="time_left_clock_icon"
+              />
+              <p className="text-red-500">
+                <span className="font-medium">5 days</span> left at this price!
+              </p>
+            </div>
+
+            <div className="flex gap-3 items-center pt-2">
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}{" "}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg text-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg text-gray-500">
+                {courseData.discount} % off
+              </p>
+            </div>
+
+            <div className="flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500">
+              <div className="flex items-center gap-1">
+                <img src={assets.star} alt="star_icon" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="clock_icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="lesson_icon" />
+                <p>{calculateNoOfLectures(courseData)}</p>
+              </div>
+            </div>
+
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">{isAlreadYEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
+
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">What's in the course?</p>
+              <ul className="ml-4 mt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Lifetime access with free updates</li>
+                <li>Step-by-step, hands-on project guidance</li>
+                <li>Download resources and source code</li>
+                <li>Quizzes to test your knowledge</li>
+                <li>Certificate of completion</li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
