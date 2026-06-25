0eeb0764c82ef14a3a3c66fa6bcb1bd045a02463
Shivam <choudharyshivam0508@gmail.com>
2026-06-25 13:09:37 +0530

    Feat: Enhance Footer and Sidebar components; add social media links and improve navigation

--- Frontend/src/components/educator/Sidebar.jsx
+++ Frontend/src/components/educator/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
 
@@ -32,7 +33,7 @@ const Sidebar = () => {
     isEducator && (
       <div className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500">
         {menuItems.map((item) => (
-          <NavLink>
+          <NavLink to={item.path} key={item.name} end={item.path === "/educator"} className={({isActive}) => `flex items-center md:flex-row flex-col md:justify-start justify-center py-3.5 md:px-10 gap-3 ${isActive ? 'bg-indigo-50 border-r-[6px] border-indigo-500/90' : 'hover:bg-gray-100/90 border-r-[6px] border-white hover:border-gray-100/90'}`}>
             <img src={item.icon} alt="" className="w-6 h-6" />
             <p className="md:block hidden text-center">{item.name}</p>
           </NavLink>
