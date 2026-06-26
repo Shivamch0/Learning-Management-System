5d31a54d7dc9a38edd502afeef627163d10368d3
Shivam <choudharyshivam0508@gmail.com>
2026-06-26 15:52:07 +0530

    Feat: Fix the updaeRoleEducator controller

--- Backend/src/controller/educator.controller.js
+++ Backend/src/controller/educator.controller.js
@@ -1,6 +1,6 @@
 import { clerkClient } from '@clerk/express'
 
-export const updateRoleEducator = async () => {
+export const updateRoleEducator = async (req , res) => {
     try {
         const userId = req.auth.userId;
 
