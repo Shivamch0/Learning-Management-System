407737906833ffb9afa94fc38bcd87414df0359a
Shivam <choudharyshivam0508@gmail.com>
2026-06-26 20:57:37 +0530

    Feat: Implement educator role update functionality in Navbar with API integration and notifications

--- Frontend/src/components/student/Navbar.jsx
+++ Frontend/src/components/student/Navbar.jsx
@@ -3,13 +3,43 @@ import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/react";
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
 
   const { openSignIn } = useClerk();
   const { user } = useUser();
-  const { navigate, isEducator } = useContext(AppContext);
+  const { navigate, isEducator, backendUrl, setIsEducator, getToken } =
+    useContext(AppContext);
+
+  const becomeEducator = async () => {
+    try {
+      if (isEducator) {
+        navigate("/educator");
+        return;
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.get(
+        backendUrl + "/api/educator/update-role",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+
+      if (data.success) {
+        setIsEducator(true);
+        toast.success(data.message)
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+        toast.error(error.message);
+    }
+  };
 
   return (
     <header
@@ -22,13 +52,14 @@ const Navbar = () => {
         className="w-28 lg:w-32 cursor-pointer"
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500">
-        <div className="flex items-center gap-5 ">
+        <div className="flex items-center gap-5 cursor-pointer">
           {user && (
             <>
-              <button onClick={() => navigate("/educator")}>
-                {" "}
+              <button onClick={becomeEducator}>
+                {""}
                 {isEducator ? "Educator Dashboard" : "Become Educator"}
               </button>
+              <p>|</p>
               <Link to="/my-enrollments">My Enrollments</Link>
             </>
           )}
@@ -46,13 +77,14 @@ const Navbar = () => {
       </div>
 
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
-        <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+        <div className="flex items-center gap-4 sm:gap-2 max-sm:text-xs">
           {user && (
             <>
-              <button onClick={() => navigate("/educator")}>
+              <button onClick={becomeEducator}>
                 {" "}
                 {isEducator ? "Educator Dashboard" : "Become Educator"}
               </button>
+              <p>|</p>
               <Link to="/my-enrollments">My Enrollments</Link>
             </>
           )}
