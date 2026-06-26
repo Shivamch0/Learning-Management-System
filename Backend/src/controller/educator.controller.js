3c397b1179e594df6f99af89101d7384f77f5ba2
Shivam <choudharyshivam0508@gmail.com>
2026-06-26 16:06:11 +0530

    Feat: Enhance updateRoleEducator function with authentication checks and logging

--- Backend/src/controller/educator.controller.js
+++ Backend/src/controller/educator.controller.js
@@ -1,16 +1,23 @@
-import { clerkClient } from '@clerk/express'
+import { clerkClient, getAuth } from "@clerk/express";
 
-export const updateRoleEducator = async (req , res) => {
-    try {
-        const userId = req.auth.userId;
+export const updateRoleEducator = async (req, res) => {
+  console.log(req.auth.userId);
+  console.log(req.auth);
+  console.log(req.headers.authorization);
+  try {
+    const { userId } = getAuth(req);
 
-        await clerkClient.users.updateUserMetadata(userId , {
-            publicMetadata : {
-                role : 'educator'
-            }
-        })
-        res.json({success : true , message: 'You can publish a course now'})
-    } catch (error) {
-        res.json({success : false , message : error.message})
+    if (!userId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
     }
-}
+
+    await clerkClient.users.updateUserMetadata(userId, {
+      publicMetadata: {
+        role: "educator",
+      },
+    });
+    res.json({ success: true, message: "You can publish a course now" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
